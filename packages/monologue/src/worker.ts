4a957c31d1236ae3041d5ad3c440e6e5c6de6d30
Matthieu Sieben <matthieu.sieben@gmail.com>
2024-10-25 14:52:47 +0200

    use @atproto/jetstream

--- packages/monologue/src/worker.ts
+++ packages/monologue/src/worker.ts
@@ -1,119 +1,103 @@
-import { CommitType, Jetstream } from '@skyware/jetstream'
-import WebSocket from 'ws'
+import { schemas } from '@atproto/api'
+import { jetstream } from '@atproto/jetstream'
 
 import { Context } from './context.js'
-import * as AppBskyActorFollow from './lexicon/types/app/bsky/graph/follow.js'
-import * as ChatBskyMonologueMessage from './lexicon/types/chat/bsky/monologue/message.js'
 
 export async function worker(signal: AbortSignal, { db }: Context) {
-  // @TODO: do not use "@skyware/jetstream" as it seems to be aimed at browser usage
-  const jetstream = new Jetstream({
-    ws: WebSocket,
-    wantedCollections: ['chat.bsky.monologue.message', 'app.bsky.graph.follow'],
-  })
-
-  jetstream.on('chat.bsky.monologue.message', async ({ did, commit }) => {
-    if (
-      (commit.operation === CommitType.Create ||
-        commit.operation === CommitType.Update) &&
-      ChatBskyMonologueMessage.isRecord(commit.record)
-    ) {
-      const author = did
-      const { subject } = commit.record
-
-      if (author !== subject) {
-        // @TODO: Ensure mutuals
-      }
-
-      const indexedAt = new Date().toISOString()
-      const createdAt = new Date(
-        commit.record.createdAt || indexedAt,
-      ).toISOString()
-
-      await db
-        .insertInto('message')
-        .values({
-          uri: commit.rkey,
-
-          author,
-          subject,
-
-          indexedAt,
-          createdAt,
-
-          record: JSON.stringify(commit.record),
-        })
-        .onConflict((oc) =>
-          oc.column('uri').doUpdateSet({
-            author,
-            subject,
-            invalidatedAt: null,
-          }),
-        )
-        .execute()
-    } else if (
-      commit.operation === CommitType.Delete ||
-      commit.operation === CommitType.Update // New record value is invalid
-    ) {
-      await db
-        .updateTable('message')
-        .set({
-          deletedAt: new Date().toISOString(),
-        })
-        .where('uri', '=', commit.rkey)
-        .execute()
-    }
-  })
-
-  jetstream.on('app.bsky.graph.follow', ({ did, commit }) => {
-    const { rkey, rev } = commit
-
-    const follower = did
-    if (
-      (commit.operation === CommitType.Create ||
-        commit.operation === CommitType.Update) &&
-      AppBskyActorFollow.isRecord(commit.record)
-    ) {
-      // Upsert value
-
-      const followee = commit.record.subject
-
-      console.log('New follow:', {
-        rkey,
-        follower,
-        followee,
-      })
-    } else if (
-      commit.operation === CommitType.Delete ||
-      commit.operation === CommitType.Update // New record value is invalid
-    ) {
-      // Remove value
-
-      console.log('Deleted follow:', {
-        rkey,
-        follower,
-      })
-    }
-  })
-
+  let cursor: number | undefined = undefined
   while (!signal.aborted) {
-    await new Promise<void>((resolve) => {
-      const close = () => {
-        // @TODO throw (reject) on repeated error
-        // @TODO backoff retries
+    try {
+      for await (const event of jetstream({
+        cursor,
+        schemas,
+        signal,
+        wantedCollections: [
+          'chat.bsky.monologue.message',
+          'app.bsky.graph.follow',
+        ],
+      })) {
+        if (event.kind !== 'commit') continue
 
-        signal.removeEventListener('abort', close)
-        jetstream.removeListener('error', close)
-        // No need to await for close event
-        // jetstream.once('close', resolve)
-        jetstream.close()
+        cursor = event.time_us
+        const { did, commit } = event
 
-        resolve()
+        if (commit.collection === 'chat.bsky.monologue.message') {
+          const author = did
+
+          if (
+            (commit.operation === 'create' || commit.operation === 'update') &&
+            commit.record.$type === 'chat.bsky.monologue.message'
+          ) {
+            const { subject } = commit.record
+
+            if (author !== subject) {
+              // @TODO: Ensure mutuals
+            }
+
+            const indexedAt = new Date().toISOString()
+            const createdAt = new Date(
+              commit.record.createdAt || indexedAt,
+            ).toISOString()
+
+            await db
+              .insertInto('message')
+              .values({
+                uri: commit.rkey,
+
+                author,
+                subject,
+
+                indexedAt,
+                createdAt,
+
+                record: JSON.stringify(commit.record),
+              })
+              .onConflict((oc) =>
+                oc.column('uri').doUpdateSet({
+                  author,
+                  subject,
+                  invalidatedAt: null,
+                }),
+              )
+              .execute()
+          } else {
+            await db
+              .updateTable('message')
+              .set({
+                deletedAt: new Date().toISOString(),
+              })
+              .where('uri', '=', commit.rkey)
+              .execute()
+          }
+        } else if (commit.collection === 'app.bsky.graph.follow') {
+          const follower = did
+          const { rkey, rev } = commit
+
+          if (
+            (commit.operation === 'create' || commit.operation === 'update') &&
+            commit.record.$type === 'app.bsky.graph.follow'
+          ) {
+            // Upsert value
+
+            const followee = commit.record.subject
+
+            console.log('New follow:', {
+              rkey,
+              follower,
+              followee,
+            })
+          } else {
+            // Remove value
+
+            console.log('Deleted follow:', {
+              rkey,
+              follower,
+            })
+          }
+        }
       }
-
-      signal.addEventListener('abort', close)
-      jetstream.addListener('error', close)
-      jetstream.start()
-    })
+    } catch (err) {
+      continue // retry
+    }
   }
 }
